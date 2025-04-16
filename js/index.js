38e776e4415371690007746caa408e595334ce6d
habte2119 <habtemariam81@gmail.com>
2025-04-16 13:44:49 +0300

    no

---
+++ js/index.js
@@ -0,0 +1,23 @@
+$(".oneone").on("click",function(){
+    alert("connected successfully bro!");
+})
+
+
+
+    $(".accordion-button1").on("click",function () {
+        // Toggle the next .accordion-content div
+        $("#collapseTwo").slideToggle();
+
+    });
+
+    
+    $(".accordion-button2").on("click",function () {
+        // Toggle the next .accordion-content div
+        $("#collapseTwo2").slideToggle();
+
+    });
+
+    $(".wahtis").on("click",function(){
+        $("#answer1").slideToggle();
+    })
+
